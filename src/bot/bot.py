890b335795028f24b4ae5959bd22552e22bdda0d
vincentve42 <vincentve42@gmail.com>
2026-02-03 09:07:06 +0700

    Scrapper Bot

---
+++ src/bot/bot.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+import mysql.connector as sql
+from dotenv import load_dotenv
+import os
+# 
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+# Sql Connect
+
+mydb = sql.connect(
+    host = "localhost",
+    user = "root",
+    database = "kidung_pemulihan"
+)
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='', intents=intents)
+
+status = -1
+
+@bot.event
+async def on_message(message):
+    
+    print(message.content)  
+    if message.content.startswith("!"):
+        argument = message.content.split(" ", 2)
+        if(argument[1] == 'status'): 
+            if argument[2] == '0' or argument[2] == "suplemen":
+                status = 0
+                print(status)
+            if argument[2] == '1' or argument[2] == "kidung":
+                status = 1
+                print(status)
+        
+    else:    
+        arr = message.content.split("\n", 1)
+        mycursor = mydb.cursor()
+
+        number = int(arr[0].replace("S.", "").strip())
+    
+        run = "INSERT INTO suplemen (judul, isi) VALUES(%s, %s)"
+        val = (number, arr[1])
+
+        mycursor.execute(run,val)
+
+        mydb.commit()
+    
+
+   
+bot.run(TOKEN)
